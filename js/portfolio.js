b0a044130ff62949405df287dd3005d42560d700
jkpoi987620-hash <jkpoi987620@gmail.com>
2026-04-13 17:24:01 +0900

    0413

--- js/portfolio.js
+++ js/portfolio.js
@@ -196,7 +196,7 @@ const workData = {
     pdf: `${BASE}/img/UNIV-EAT_PORTFOLIO.png`,
     link: `${BASE}/portfolio_ex/dayflow_index/dayflow_main.html`,
     bg: `${BASE}/img/UNIV_CTA-bg.png`,
-    text: '본 프로젝트는 Figma로 디자인하고 HTML, CSS, JavaScript로 구현되었습니다.'
+    text: '본 프로젝트는 아직 구현되지 않았습니다.'
   },
 };
 
